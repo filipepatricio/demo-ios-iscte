d46f74e8f77a9099aceb5e8d9880a011057b26ff
Filipe Patricio <filipepatricio@Filipes-MBP.lan>
2016-04-23 15:00:21 +0100

    Create project

---
+++ Demo-ios-iscte/Demo-ios-iscte/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo-ios-iscte
+//
+//  Created by Filipe Patricio on 23/04/16.
+//  Copyright © 2016 Filipe Patricio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo-ios-iscte/Demo-ios-iscte/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo-ios-iscte
+//
+//  Created by Filipe Patricio on 23/04/16.
+//  Copyright © 2016 Filipe Patricio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
